5f756ff0565627c02a28fb01de7c4d45d35d5cf8
r.molina <molinareymon@rocketmail.com>
2021-05-27 14:56:10 -0700

    git init

---
+++ src/testing/squareTest.java
@@ -0,0 +1,17 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class squareTest {
+
+	@Test
+	void test() {
+		JUnitTesting test = new JUnitTesting();
+		int output = test.square(5);
+		assertEquals(25, output);
+	}
+	
+
+}
